145cfa6b56949c0bad89307bb799c826caf4230a
Shivam sood <155950691+Shivam-sood63@users.noreply.github.com>
2025-07-31 22:21:18 +0530

    Update MySkills.jsx

--- src/Components/MySkills.jsx
+++ src/Components/MySkills.jsx
@@ -11,7 +11,7 @@ const MySkills = () => {
       <div className="w-full">
         <div className="flex w-full justify-center px-6 py-2">
           <div className="w-52 rounded mx-4  md:w-44 md:m-10 sm:w-32 sm:px-0  bg-gray-800 hover:bg-linear-65 from-purple-500 to-blue-600 cursor-pointer transition delay-3  ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 items-center">
-            <img className="p-1" src="src/assets/shivamh.png" alt="" />
+            <img className="p-1" src="src\assets\shivamh.png" alt="" />
             <h1 className="text-center font-bold pb-2 ">HTML</h1>
           </div>
           <div className="w-52 rounded mx-4  md:w-44 md:m-10 sm:w-32 sm:px-0  bg-gray-800  items-center hover:bg-linear-65 from-purple-500 to-blue-600 cursor-pointer transition delay-3  ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500">
