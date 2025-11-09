b681becb0178e462965990d67c1f4458eb3fe06d
Shivam sood <155950691+Shivam-sood63@users.noreply.github.com>
2025-11-09 10:32:53 +0530

    img update

--- src/Components/MySkills.jsx
+++ src/Components/MySkills.jsx
@@ -3,7 +3,10 @@ import mh1 from "../assets/shivamh.png"
 import mh2 from "../assets/shivamc.png"
 import mh3 from "../assets/shivamj.png"
 import mh4 from "../assets/shivamr.png"
-
+import mh5 from "../assets/shivamr.png"
+import mh6 from "../assets/shivamr.png"
+import mh7 from "../assets/shivamr.png"
+import mh8 from "../assets/shivamr.png"
 const MySkills = () => {
   return (
     <div className="bg-black text-white w-full">
@@ -36,22 +39,22 @@ const MySkills = () => {
 
         <div className="flex w-full justify-center px-6 py-2">
           <div className="w-52 rounded mx-4  md:w-44 md:m-10 sm:w-32 sm:px-0  bg-gray-800  items-center hover:bg-linear-65 from-purple-500 to-blue-600 cursor-pointer transition delay-3  ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500">
-            <img className="p-1" src="src/assets/shivamt.png" alt="" />
+            <img className="p-1" src={mh5} alt="" />
             <h1 className="text-center font-bold pb-2 ">Tailwind</h1>
           </div>
           <div className="w-52 rounded mx-4  md:w-44 md:m-10 sm:w-32 sm:px-0  bg-gray-800  items-center hover:bg-linear-65 from-purple-500 to-blue-600 cursor-pointer transition delay-3  ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500">
-            <img className="p-1" src="src/assets/shivamg.png" alt="" />
+            <img className="p-1" src={mh6} alt="" />
             <h1 className="text-center font-bold pb-2 ">Git</h1>
           </div>
           <div className="w-52 rounded mx-4  md:w-44 md:m-10 sm:w-32 sm:px-0  bg-gray-800  items-center hover:bg-linear-65 from-purple-500 to-blue-600 cursor-pointer transition delay-3  ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500">
-            <img className="p-1" src="src/assets/shivamgh.png" alt="" />
+            <img className="p-1" src={mh7} alt="" />
             <h1 className="text-center font-bold pb-2 ">GitHub</h1>
           </div>
         </div>
 
         <div className="flex w-full justify-center px-32 py-1 md:px-4 md:py-4 ">
           <div className="w-52 rounded mx-4  md:w-44 md:m-10 sm:px-0  bg-gray-800  items-center justify-center text-center hover:bg-linear-65 from-purple-500 to-blue-600 cursor-pointer transition delay-3  ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500">
-            <img className="items-center p-1 md:px-5" src="src/assets/shivamn.png" alt="" />
+            <img className="items-center p-1 md:px-5" src={mh8} alt="" />
             <h1 className="text-center font-bold pb-2 ">Node</h1>
           </div>
         </div>
